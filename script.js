bd4648aac9fd60026ab0b3e4b96fcdac31bf06dc
saikrishnadatt3842 <102149703+saikrishnadatt3842@users.noreply.github.com>
2022-03-22 15:40:31 +0530

    saudatt
    haiiiiii

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("hello");
+
+const myname ="sai datt";
+console.log(myname);
+const gs = "fff";
+
+console.log(gs);
+
+ console.log(gs);
+
+ 
+
+ const h3 = document.querySelector("#div1");
+h3.addEventListener("click", function() {
+h3.textContent = myname;
+ h3.style.backgroundcolor = "red";
+ h3.style.padding = "5ren";
+});
+function clickablediv(){
+   document.getElementById("div1").style.background="red";}
